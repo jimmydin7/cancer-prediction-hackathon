e9e30304517a81854c50ff781783089b4349acc7
jimmydin7 <turnspeaker@gmail.com>
2025-08-24 22:41:38 +0300

    remove bad cursor

--- static/js/index.js
+++
@@ -1,5 +0,0 @@
-const cursor = document.querySelector(".custom-cursor");
-document.addEventListener("mousemove", (e) => {
-  cursor.style.top = e.clientY + "px";
-  cursor.style.left = e.clientX + "px";
-});
